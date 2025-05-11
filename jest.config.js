0e77cc1aabb9ac693d7b50ad6b515a4e56574fba
Mehluli D <mehluliseries@outlook.com>
2025-05-11 21:15:38 +0200

    Improvements #49

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
+/** @type {import('jest').Config} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(test).ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testMatch: ['**/tests/unit/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/']
 };
